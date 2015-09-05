63f4a315130e2c1a739c38dbecc85cccf54d9b4c
Chris Clifton <chris9361@yahoo.com>
2015-09-05 00:01:47 -0500

    new files

---
+++ SayMyName.py
@@ -0,0 +1,10 @@
+# SayMyName.py - prints a screen full of the user's name
+__author__ = 'Chris'
+
+# Ask the user for their name
+name = input("What is your name? ")
+
+# Print their name 100 times
+for x in range(100):
+    # Print their name followed by a space, not a new line
+    print(name, end = " rules! ")
